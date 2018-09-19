a7a580f3ecc09f62f8e265b60c8609752b888d3c
Martin Archer <bitclef@gmail.com>
2018-09-19 18:08:48 -0400

    Animated Earth with star background. Slept some threads so it doesn't use too much CPU haha.

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -6,7 +7,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Program extends Application {
@@ -15,27 +18,41 @@ public class Program extends Application {
     }
 
     public void start(Stage theStage){
-        theStage.setTitle("Canvas Example");
+        theStage.setTitle("Hello World GUI!");
 
         Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
 
-        Canvas canvas = new Canvas(450, 250);
+        Canvas canvas = new Canvas(700, 400);
         root.getChildren().add(canvas);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        gc.setFill(Color.SKYBLUE);
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(2);
-        Font theFont = Font.font( "Georgia", FontWeight.BOLD, 48);
-        gc.setFont(theFont);
-        gc.fillText("Hello World!", 60, 50);
-        gc.strokeText("Hello World!", 60, 50);
+        Image earth = new Image("world.gif");
+        Image stars = new Image("stars.jpeg");
+        gc.drawImage(stars, 0, 0);
+        new AnimationTimer(){
+            public void handle(long currentNanoTime){
 
-        Image earth = new Image("world.png");
-        gc.drawImage(earth, 180, 75);
+                gc.drawImage(earth, 200, 100);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+
+        Text text = new Text();
+        text.setFont(Font.font("Georgia", FontWeight.BOLD, FontPosture.REGULAR, 48));
+        text.setFill(Color.SKYBLUE);
+        text.setStrokeWidth(2);
+        text.setStroke(Color.BLACK);
+        text.setText("Hello World!");
+        text.setX(200); text.setY(70);
+        root.getChildren().add(text);
 
         theStage.show();
     }
