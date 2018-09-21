505da3894cb8ed19040d82151c8e227f1c1ff15f
Martin Archer <bitclef@gmail.com>
2018-09-21 19:43:31 -0400

    Refactored Spagetti Code

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,11 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -18,21 +15,24 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import javax.sound.midi.*;
-import java.io.File;
 import java.io.IOException;
 
 public class Program extends Application {
+
+    private Sequencer sequencer;
+    private int[] earthX = {200};
+    private int[] earthY = {100};
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage theStage) throws InvalidMidiDataException, IOException, MidiUnavailableException {
+    @Override
+    public void start(Stage theStage) {
         theStage.setTitle("Martin's World!");
-
         Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
-
         Canvas canvas = new Canvas(700, 400);
         root.getChildren().add(canvas);
 
@@ -41,26 +41,38 @@ public class Program extends Application {
         Image earth = new Image("pictures/world.gif");
         Image stars = new Image("pictures/stars.jpeg");
 
+        animationTimer(gc, earth, stars);
 
-        final int[] x = {200};
-        final int[] y = {100};
+        midiSequencer("audio/emtion.mid", 100, 100);
 
-        theScene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.UP) {
-                y[0] -= 2;
-            } else if (event.getCode() == KeyCode.DOWN) {
-                y[0] += 2;
-            } else if (event.getCode() == KeyCode.LEFT) {
-                x[0] -= 2;
-            } else if (event.getCode() == KeyCode.RIGHT) {
-                x[0] += 2;
-            }
-        });
+        screenText(root, "Hello World!", 200, 60, 48);
+        screenText(root, "Use the Arrow Keys to move the world!", 100, 95, 24);
+
+        keyPressed(theScene);
+
+        theStage.show();
+        theStage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::closeWindowEvent);
+    }
+
+
+    private void closeWindowEvent(WindowEvent event) {
+        sequencer.close();
+            midiSequencer("audio/163.mid", 150, 1);
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+    }
+
+
+    private void animationTimer(GraphicsContext gc, Image image, Image image2) {
         new AnimationTimer() {
             public void handle(long currentNanoTime) {
 
-                gc.drawImage(stars, 0, 0);
-                gc.drawImage(earth, x[0], y[0]);
+                gc.drawImage(image2, 0, 0);
+                gc.drawImage(image, earthX[0], earthY[0]);
 
                 try {
                     Thread.sleep(100);
@@ -70,39 +82,60 @@ public class Program extends Application {
 
             }
         }.start();
+    }
 
-        Sequence sequence = MidiSystem.getSequence(getClass().getResource("audio/wet.mid"));
-        Sequencer sequencer = MidiSystem.getSequencer();
-        sequencer.open();
-        sequencer.setSequence(sequence);
-        sequencer.setTempoInBPM(85);
-        sequencer.setLoopCount(100);
+    private void keyPressed(Scene theScene) {
+        theScene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.UP) {
+                earthY[0] -= 2;
+            } else if (event.getCode() == KeyCode.DOWN) {
+                earthY[0] += 2;
+            } else if (event.getCode() == KeyCode.LEFT) {
+                earthX[0] -= 2;
+            } else if (event.getCode() == KeyCode.RIGHT) {
+                earthX[0] += 2;
+            }
+        });
+    }
+
+    private void midiSequencer(String location, int tempo, int loopCount) {
+        try {
+            sequencer = MidiSystem.getSequencer();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+        Sequence sequence = null;
+        try {
+            sequence = MidiSystem.getSequence(getClass().getResource(location));
+        } catch (InvalidMidiDataException | IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            sequencer.open();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+        try {
+            sequencer.setSequence(sequence);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        sequencer.setTempoInBPM(tempo);
+        sequencer.setLoopCount(loopCount);
         sequencer.start();
 
+
+    }
+
+    private void screenText(Group root, String visualText, int setX, int setY, int size) {
         Text text = new Text();
-        text.setFont(Font.font("Georgia", FontWeight.BOLD, FontPosture.REGULAR, 48));
+        text.setFont(Font.font("Georgia", FontWeight.BOLD, FontPosture.REGULAR, size));
         text.setFill(Color.SKYBLUE);
         text.setStrokeWidth(2);
         text.setStroke(Color.BLACK);
-        text.setText("Hello World!");
-        text.setX(200);
-        text.setY(60);
+        text.setText(visualText);
+        text.setX(setX);
+        text.setY(setY);
         root.getChildren().add(text);
-
-        Text subtext = new Text();
-        subtext.setFont(Font.font("Georgia", FontWeight.BOLD, FontPosture.REGULAR, 24));
-        subtext.setFill(Color.SKYBLUE);
-        subtext.setStrokeWidth(2);
-        subtext.setStroke(Color.BLACK);
-        subtext.setText("Use the Arrow Keys to move the world!");
-        subtext.setX(100);
-        subtext.setY(95);
-        root.getChildren().add(subtext);
-
-        theStage.show();
-        theStage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::closeWindowEvent);
-    }
-    private void closeWindowEvent(WindowEvent event){
-        System.exit(1);
     }
 }
